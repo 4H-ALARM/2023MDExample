9dbb0a2d7b2d1967b0b2c9821a7d1156ba8544ed
atk <35353916+arnoldtkalmbach@users.noreply.github.com>
2023-01-10 09:30:13 -0500

    initial mecanum example

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;;
+
+/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
+public class Robot extends TimedRobot {
+  private static final int kFrontLeftChannel = 2;
+  private static final int kRearLeftChannel = 3;
+  private static final int kFrontRightChannel = 1;
+  private static final int kRearRightChannel = 0;
+
+  private static final int kJoystickChannel = 0;
+
+  private MecanumDrive m_robotDrive;
+  private Joystick m_stick;
+
+  @Override
+  public void robotInit() {
+    WPI_VictorSPX frontLeft = new WPI_VictorSPX(kFrontLeftChannel);
+    WPI_VictorSPX rearLeft = new WPI_VictorSPX(kRearLeftChannel);
+    WPI_VictorSPX frontRight = new WPI_VictorSPX(kFrontRightChannel);
+    WPI_VictorSPX rearRight = new WPI_VictorSPX(kRearRightChannel);
+
+    // Invert the right side motors.
+    // You may need to change or remove this to match your robot.
+    frontRight.setInverted(true);
+    rearRight.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+    m_stick = new Joystick(kJoystickChannel);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Use the joystick X axis for forward movement, Y axis for lateral
+    // movement, and Z axis for rotation.
+    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+  }
+}
